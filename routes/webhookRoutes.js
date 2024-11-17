40cca463c4e912f4b286680f98a71f1595e41ca8
Aguero Alejandro Aalinsvid Andrade <alejandro-aalinsvid-andrade.aguero@a1.group>
2024-11-17 15:14:21 -0600

    fix swagger post webhook

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -13,7 +13,7 @@ const webhookController = require('../controllers/webhookController');
 /**
  * @swagger
  * /webhook/{nombre_clave_webhook}:
- *   get:
+ *   post:
  *     summary: Recibir un webhook
  *     tags: [Webhooks]
  *     parameters:
@@ -50,6 +50,6 @@ const webhookController = require('../controllers/webhookController');
  *       500:
  *         description: Error al almacenar el webhook
  */
-router.get('/:nombre_clave_webhook', webhookController.authenticate, webhookController.receiveWebhook);
+router.post('/:nombre_clave_webhook', webhookController.authenticate, webhookController.receiveWebhook);
 
 module.exports = router;
